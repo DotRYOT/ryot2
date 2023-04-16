06d5cd25f5807dcb4049f00a5e012ee326b7fd31
RYOT <114276482+DotRYOT@users.noreply.github.com>
2023-04-16 03:09:44 -0500

    5

--- Main/index.js
+++ Main/index.js
@@ -4,3 +4,12 @@ const sideNav = document.querySelector(".side_nav");
 menuButton.addEventListener("click", () => {
   sideNav.classList.toggle("open");
 });
+
+var button = document.getElementById("classyCatLodgeButton");
+
+button.addEventListener("click", function () {
+  window.open(
+    "https://vrchat.com/home/world/wrld_8183b4f4-55d5-4572-bb8c-45fd93b32c07",
+    "_blank"
+  );
+});
